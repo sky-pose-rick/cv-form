69b59aad89be3f3db7712d197544b5d1ce27e4d1
Richard Cranwell <rickycranwell@gmail.com>
2021-12-12 14:56:45 -0600

    add eslint to project files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+/* eslint-disable no-useless-constructor */
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      CV form App.
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        CV form App
+      </div>
+    );
+  }
 }
 
 export default App;
