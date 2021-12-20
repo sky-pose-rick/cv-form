f88116e36581c3df753a141acea541dcf694d728
Richard Cranwell <rickycranwell@gmail.com>
2021-12-20 14:47:34 -0600

    make input field into a functional component

--- src/components/InputtableField.js
+++ src/components/InputtableField.js
@@ -1,45 +1,32 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable no-useless-constructor */
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React from 'react';
 import uniqid from 'uniqid';
 
 // eslint-disable-next-line no-unused-vars
 import styles from '../styles/inputs.css';
 
-class InputtableField extends Component {
-  constructor(props) {
-    super(props);
+// eslint-disable-next-line func-names
+const InputtableField = function (props) {
+  const {
+    label,
+    value,
+    editable,
+    onChange,
+  } = props;
 
-    this.id = uniqid();
-  }
+  const myId = uniqid();
 
-  // should have two different appearances: editing open or editing close
-  render() {
-    const {
-      label,
-      value,
-      editable,
-      onChange,
-    } = this.props;
-    // console.log(this.props);
-    if (editable) {
-      return (
-        <div className="Inputtable-Field">
-          <label htmlFor={this.id}>
-            {`${label}: `}
-          </label>
-          <input id={this.id} type="text" value={value} onChange={onChange} />
-        </div>
-      );
-    }
-
-    return (
-      <div className="Inputtable-Field">
-        <p>{`${label}: ${value}`}</p>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="Inputtable-Field">
+      {editable && (
+      <label htmlFor={myId}>
+        {`${label}: `}
+      </label>
+      )}
+      {editable && <input id={myId} type="text" value={value} onChange={onChange} />}
+      {!editable && <p>{`${label}: ${value}`}</p>}
+    </div>
+  );
+};
 
 export default InputtableField;
