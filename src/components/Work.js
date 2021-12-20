8cf68f43147a950135306b24998a10160acd1ad1
Richard Cranwell <rickycranwell@gmail.com>
2021-12-20 16:17:37 -0600

    make work into a functional component

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,97 +1,67 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable no-useless-constructor */
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import uniqid from 'uniqid';
 
 import WorkItem from './WorkItem';
 // eslint-disable-next-line no-unused-vars
 import styles from '../styles/panel.css';
 
-class Work extends Component {
-  constructor(props) {
-    super(props);
+// eslint-disable-next-line func-names
+const Work = function (props) {
+  const { content, onSubmit } = props;
 
-    const { content } = this.props;
+  const [jobs, setJobs] = useState(content && content.jobs ? content.jobs : {});
 
-    if (content) {
-      this.state = {
-        jobs: content.jobs ? content.jobs : {},
-      };
-    } else {
-      this.state = {
-        jobs: {},
-      };
-    }
-
-    this.addJob = this.addJob.bind(this);
-  }
-
-  addJob() {
+  const addJob = () => {
     const newKey = uniqid();
+    const shallowCopy = { ...jobs };
+    shallowCopy[newKey] = {};
+    setJobs(shallowCopy);
+  };
 
-    this.setState((state) => {
-      const stateCopy = state;
-      stateCopy.jobs[newKey] = {};
-      return stateCopy;
-    });
-  }
-
-  makeOnChange(parentFunc, key) {
-    // have this component insert the key, let child pass the degree
-    const onChange = (degree) => {
-      this.setState((state) => {
-        const stateCopy = state;
-        stateCopy.jobs[key] = degree;
-        // pass new state to parent
-        parentFunc(stateCopy);
-        return stateCopy;
-      });
-    };
-
-    return onChange;
-  }
-
-  makeDelete(parentFunc, key) {
+  const makeDelete = (parentFunc, key) => {
     const onDelete = () => {
-      this.setState((state) => {
-        const stateCopy = state;
-        delete stateCopy.jobs[key];
-        // pass new state to parent
-        parentFunc(stateCopy);
-        return stateCopy;
-      });
+      const shallowCopy = { ...jobs };
+      delete shallowCopy[key];
+      setJobs(shallowCopy);
+      parentFunc(shallowCopy);
     };
 
     return onDelete;
-  }
+  };
 
-  render() {
-    const { onSubmit } = this.props;
-    const { jobs } = this.state;
-    const degreeArray = Object.entries(jobs);
+  const makeOnChange = (parentFunc, key) => {
+    // have this component insert the key, let child pass the degree
+    const onChange = (degree) => {
+      const shallowCopy = { ...jobs };
+      shallowCopy[key] = degree;
+      setJobs(shallowCopy);
+      parentFunc(shallowCopy);
+    };
 
-    return (
-      <div className="Work">
-        <h2>Work Experience</h2>
-        {degreeArray.map((entry) => {
-          const changeFunc = this.makeOnChange(onSubmit, entry[0]);
-          const deleteFunc = this.makeDelete(onSubmit, entry[0]);
-          return (
-            <WorkItem
-              key={entry[0]}
-              content={entry[1]}
-              onSubmit={changeFunc}
-              onDelete={deleteFunc}
-            />
-          );
-        })}
-        <div className="item-add">
-          <button type="button" onClick={this.addJob}>Add New Work Entry</button>
-        </div>
+    return onChange;
+  };
+
+  return (
+    <div className="Work">
+      <h2>Work Experience</h2>
+      {Object.entries(jobs).map((entry) => {
+        const changeFunc = makeOnChange(onSubmit, entry[0]);
+        const deleteFunc = makeDelete(onSubmit, entry[0]);
+        return (
+          <WorkItem
+            key={entry[0]}
+            content={entry[1]}
+            onSubmit={changeFunc}
+            onDelete={deleteFunc}
+          />
+        );
+      })}
+      <div className="item-add">
+        <button type="button" onClick={addJob}>Add New Work Entry</button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Work;
